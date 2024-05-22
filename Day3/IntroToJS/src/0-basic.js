53757f5278724094c703c9580e2ddb80c6674382
theDarkShadow <rayyan9290@gmail.com>
2024-05-22 14:24:40 +0530

    day-3 commits

---
+++ Day3/IntroToJS/src/0-basic.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+
+
+// Data Types : String, Number, Boolean, Null, Undefined, Symbol
+// Variables : var, let, const
+// Containers : Array, Object
+// Functions : function, arrow function
+// Callbacks : Higher order functions
+// Objects
+// OOPS : Classes, Objects, Inheritance, Polymorphism, Encapsulation
+
+
+// Asynchronous Programming
+// Promises
+
+
+
+
+
---
+++ Day3/IntroToTailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  //   content: [
+  //   './src/app/**/*.{js,ts,jsx,tsx}',
+  //   './src/pages/**/*.{js,ts,jsx,tsx}',
+  //   './src/components/**/*.{js,ts,jsx,tsx',
+  //   './src/hooks/**/*.{js,ts,jsx,tsx}',
+  // ],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
